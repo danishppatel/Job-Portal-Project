3c2fb8b48d3ce88e939adb7872ebded4b737251a
brijpatel-2003 <brijpatidar2003@gmail.com>
2024-03-14 00:09:28 +0530

    Changes applied for router.jsx and main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,21 +17,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-// import ReactDOM from 'react-dom/client'
-// import './index.css'
-// import { RouterProvider } from 'react-router-dom'
-// import router from './Router/Router.jsx'
-// import { Provider } from 'react-redux'
-// import { persistor, store } from './Store.js'
-// import { PersistGate } from 'redux-persist/integration/react'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <PersistGate loading={null} persistor={persistor}>
-//          <RouterProvider router={router}/>
-//     </PersistGate>
-//   </Provider>
-  
-// )
-
